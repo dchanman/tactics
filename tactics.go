1898226f2d929a5d35b1391d51db603aa492ec81
Derek Chan <derekchan1994@gmail.com>
2017-06-15 08:54:17 +0800

    Initial commit

---
+++ tactics.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	log = logrus.WithField("app", "tactics")
+)
+
+func main() {
+	log.Println("Initializing")
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.WithField("PORT", port).Fatal("$PORT must be set")
+	}
+
+	log.Println("Hello World")
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	log.Println(http.ListenAndServe(":"+port, nil))
+	log.Println("Hello World Done")
+}
