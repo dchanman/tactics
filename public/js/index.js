c399986d8ea773e4d0152def09f8e32a5eb19983
Derek Chan <derekchan1994@gmail.com>
2017-06-22 23:46:11 +0800

    Hooked up REST API for renderer's JSON

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,24 @@
-console.log("Hello World!");
-var board = new Board(document.getElementById("game"));
-board.render("yo");
+window.Main = (function () {
+    "use strict";
+    console.log("Hello World!");
+    var Main = function () {
+        this.board = new Board(document.getElementById("game"));
+    };
+    Main.prototype.refresh = function () {
+        var xmlHttp = new XMLHttpRequest(),
+            board = this.board;
+        xmlHttp.onreadystatechange = function () {
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+                board.render(xmlHttp.responseText);
+            }
+        };
+        xmlHttp.open("GET", "/game", true);
+        xmlHttp.send(null);
+    };
+    Main.prototype.start = function () {
+        this.refresh();
+    };
+    return Main;
+}());
+var main = new Main();
+main.start();
