40f7e8819b015d142722c894a13e8083afe5fd01
Derek Chan <derekchan1994@gmail.com>
2017-07-21 15:21:40 -0700

    Created JS API wrappers

---
+++ public/js/api.js
@@ -0,0 +1,72 @@
+window.Api = (function () {
+    "use strict";
+    function createNewWebsocket(api) {
+        var uri = (window.location.protocol === "https:") ? "wss://" : "ws://",
+            ws = new WebSocket(uri + window.location.host + "/ws");
+        ws.onopen = function () {
+            api.onready();
+        };
+        ws.onmessage = function (event) {
+            var data = JSON.parse(event.data),
+                callback;
+            if (data && data.id !== null) {
+                callback = api.sentmsgs[data.id];
+                api.sentmsgs[data.id] = null;
+                if (callback) {
+                    callback(data.result, data.error);
+                }
+            }
+        };
+        ws.onclose = function () {
+            api.onclose();
+        };
+        ws.onerror = function (err) {
+            api.onerror(err);
+        };
+        return ws;
+    }
+    function Api() {
+        this.ws = createNewWebsocket(this);
+        this.sentmsgs = {};
+        this.currid = 1;
+        // Handler functions
+        this.onready = function () { return; };
+        this.onerror = function () { return; };
+        this.onclose = function () { return; };
+    }
+    function sendmsg(api, method, params, callback) {
+        var call = {
+            "id": api.currid,
+            "method": method,
+            "params": params
+        };
+        api.sentmsgs[api.currid] = callback;
+        api.currid += 1;
+        api.ws.send(JSON.stringify(call));
+    }
+    Api.prototype.hello = function () {
+        var api = this;
+        return new Promise(function (resolve, reject) {
+            sendmsg(api, "TacticsApi.Hello", [], function (result, err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    };
+    Api.prototype.getGame = function () {
+        var api = this;
+        return new Promise(function (resolve, reject) {
+            sendmsg(api, "TacticsApi.GetGame", [], function (result, err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    };
+    return Api;
+}());
--- public/js/index.js
+++ public/js/index.js
@@ -1,61 +1,33 @@
 window.Main = (function () {
     "use strict";
-    console.log("Hello World!");
     function Main() {
         this.board = new Board(document.getElementById("game"));
-        this.ws = this.createNewWebsocket();
-        this.sentmsgs = {};
-    }
-    Main.prototype.createNewWebsocket = function () {
-        var main = this,
-            uri = (window.location.protocol === "https:") ? "wss://" : "ws://",
-            ws = new WebSocket(uri + window.location.host + "/ws"),
-            currid = 0,
-            sentmsgs = {};
-        ws.onopen = function () {
+        this.api = new Api();
+        var main = this;
+        this.api.onready = function () {
             main.start();
         };
-        ws.onmessage = function (event) {
-            var data = JSON.parse(event.data),
-                callback;
-            if (data && data.id) {
-                callback = sentmsgs[data.id];
-                if (callback) {
-                    callback(data.result, data.error);
-                }
-            }
+        this.api.onerror = function (err) {
+            console.log(err);
         };
-        ws.onclose = function () {
-            console.log("Websocket closed");
+        this.api.onclose = function () {
+            console.log("API closed");
         };
-        ws.onerror = function (err) {
-            console.log("Error: " + err);
-        };
-        ws.sendmsg = function (method, params, callback) {
-            var call = {
-                "id": currid,
-                "method": method,
-                "params": params
-            };
-            sentmsgs[currid] = callback;
-            currid += 1;
-            this.send(JSON.stringify(call));
-        };
-        return ws;
-    };
+    }
     Main.prototype.refresh = function () {
         var main = this;
-        this.ws.sendmsg("TacticsApi.GetGame", [], function (result, err) {
-            if (!err) {
+        this.api.getGame()
+            .then(function (result) {
                 main.board.render(result.game);
-            }
-        });
+            });
     };
     Main.prototype.start = function () {
-        this.ws.sendmsg("TacticsApi.Hello", [], function () {
-            console.log("Received resonse to Hello!");
-        });
-        this.refresh();
+        var main = this;
+        this.api.hello()
+            .then(function () {
+                console.log("Connected to server!");
+                main.refresh();
+            });
     };
     return Main;
 }());
