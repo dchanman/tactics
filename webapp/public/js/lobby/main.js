ca7fffa854b9b5485db0f8911c58df4692158752
Derek Chan <derekchan1994@gmail.com>
2017-08-17 00:16:42 -0700

    Able to create small and large games from the lobby

--- webapp/public/js/lobby/main.js
+++ webapp/public/js/lobby/main.js
@@ -10,32 +10,68 @@ window.Main = (function () {
         }
         return s;
     }
-    Main.prototype.getGameIDs = function () {
-        var main = this;
-        $.ajax({
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            type: "POST",
-            async: true,
-            'url': '/data/',
-            'data': '{ "jsonrpc": "2.0", "method": "Server.GetGameIds", "id": 1 , "params": []}',
-            'dataType': 'json',
-            'success': function (result) {
-                main.createGameList(result.result.gameids);
-            },
-            'error': function (data) {
-                console.log(data);
-            }
+    function ajax(data) {
+        var d = JSON.stringify(data);
+        return new Promise(function (resolve, reject) {
+            $.ajax({
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                type: "POST",
+                async: true,
+                'url': '/data/',
+                'data': d,
+                'dataType': 'json',
+                'success': function (result) {
+                    resolve(result);
+                },
+                'error': function (err) {
+                    reject(err);
+                }
+            });
         });
+    }
+    Main.prototype.getGameIDs = function () {
+        var main = this,
+            jsonrpc = {
+                "id": 1,
+                "jsonrpc": "2.0",
+                "method": "Server.GetGameIds",
+                "params": []
+            };
+        ajax(jsonrpc)
+            .then(function (result) {
+                main.createGameList(result.result.gameids);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    };
+    Main.prototype.createGame = function (gameSize) {
+        var jsonrpc = {
+                "id": 1,
+                "jsonrpc": "2.0",
+                "method": "Server.CreateGame",
+                "params": [{"gameType": gameSize}]
+            };
+        ajax(jsonrpc)
+            .then(function (result) {
+                window.location.href = "/g/" + zeropad(result.result.gameid);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     };
     Main.prototype.createGameList = function (gameIDs) {
         var i, li;
+        for (i = 0; i < gameIDs.length; i += 1) {
+            gameIDs[i] = zeropad(gameIDs[i]);
+        }
         gameIDs.sort();
         for (i = 0; i < gameIDs.length; i += 1) {
             li = document.createElement("li");
-            $(li).html('<a href="/g/' + zeropad(gameIDs[i]) + '">' + zeropad(gameIDs[i]) + '</a>');
+            $(li).html('<a href="/g/' + gameIDs[i] + '">' + gameIDs[i] + '</a>');
             $("#gameList").append(li);
         }
     };
@@ -49,5 +85,11 @@ window.Main = (function () {
 $(document).ready(function () {
     "use strict";
     var main = new Main();
+    $("#gameCreateSmall").click(function () {
+        main.createGame("small");
+    });
+    $("#gameCreateLarge").click(function () {
+        main.createGame("large");
+    });
     main.run();
 });
