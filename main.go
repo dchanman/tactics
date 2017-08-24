334c5b9b70c6dfb12100f30d7619de252b414c98
Derek Chan <derekchan1994@gmail.com>
2017-08-24 01:42:31 -0500

    Templated overlay settings

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ var (
 		"./webapp/private/partials/commonjs.tmpl",
 		"./webapp/private/partials/head.tmpl",
 		"./webapp/private/partials/nav.tmpl",
+		"./webapp/private/partials/overlaysetting.tmpl",
 		"./webapp/private/partials/status.tmpl"))
 )
 
